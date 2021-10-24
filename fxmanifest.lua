32e704bffb38173faa156ff84b91eb67a4aacaac
VikingCat32 <63554052+VikingCat32@users.noreply.github.com>
2021-10-24 01:11:04 -0400

    Update fxmanifest.lua
    Updated needed information.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,17 +1,29 @@
+------------------------------------------------------------------------------------------------
+
 fx_version 'cerulean'
 games { 'gta5' };
 
-author 'Chicle <chicle@protonmail.com> - www.github.com/Eschiclers'
-name 'Pause menu information'
-description 'Script to display character information in pause menu'
-version '1.0.0'
+------------------------------------------------------------------------------------------------
+
+name 'Character-Pause-Menu-Info'
+description 'Character Information in Pause Menu'
+authors 'kstrachan418 & VikingCat32 - Forked from Eschiclers'
+version 'v1.0.0'
+repository 'https://github.com/VikingCat32/Character-Pause-Menu-Info/'
+forkedrepository 'https://github.com/Eschiclers/ch_pause_menu'
+
+------------------------------------------------------------------------------------------------
 
 client_scripts {
     'config.lua',
     'client/main.lua'
 }
 
+------------------------------------------------------------------------------------------------
+
 server_scripts {
   'config.lua',
   'server/main.lua'
 }
+
+------------------------------------------------------------------------------------------------
